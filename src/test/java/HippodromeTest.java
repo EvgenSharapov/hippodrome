96fbb6d68d22289b89337917db05047a2fda4516
EvgenSharapov <dooplert@yandex.ru>
2024-08-15 23:04:27 +0300

    horse_hippo_main test+log

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,10 +1,6 @@
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -7,15 +7,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 import java.util.stream.Stream;
-
-import static java.nio.file.Files.move;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HorseTest {
